c510664d506f195af531fc39ea45150a5bb4460d
haradan <haradan@users.noreply.github.com>
2022-02-11 14:49:23 +0000

    Configure Terraform & GitHub Actions

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "haradan"
+
+    workspaces {
+      tags = ["nisei-cloud"]
+    }
+  }
+}
+
+resource "random_pet" "test" {}
+
+output "test-pet" {
+  value = "${random_pet.test.id}"
+}
